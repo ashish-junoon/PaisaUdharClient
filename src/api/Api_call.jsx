4374e807761410b784cb0e5c53e6f3eb0edb76cb
RohitKoliii <rohit.koli@junooncapital.in>
2026-04-24 18:12:29 +0530

    no-cache

--- src/api/Api_call.jsx
+++ src/api/Api_call.jsx
@@ -5,6 +5,7 @@ const api = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
     headers: {
         "Content-Type": "application/json",
+        "Cache-Control": "no-cache"
     },
     // maxRedirects: 0, // Prevent Axios from following redirects to HTTPS
 });
